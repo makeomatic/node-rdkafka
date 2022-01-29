8fefe7818e9cced4d83f9960f9cbeef8b70419b1
AVVS <v@makeomatic.ca>
2022-01-28 17:13:16 -0800

    feat: node-pre-gyp

--- librdkafka.js
+++ librdkafka.js
@@ -7,6 +7,9 @@
  * of the MIT license.  See the LICENSE.txt file for details.
  */
 
-var kafka = require('bindings')('node-librdkafka');
+const binary = require('@mapbox/node-pre-gyp');
+const path = require('path');
+const binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+const binding = require(binding_path);
 
-module.exports = kafka;
+module.exports = binding;
