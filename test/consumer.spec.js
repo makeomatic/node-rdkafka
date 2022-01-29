3ff758de48caede0111ac7a8a5047f271a53cd57
AVVS <v@makeomatic.ca>
2022-01-29 11:54:10 -0800

    chore: use correct path

--- test/consumer.spec.js
+++ test/consumer.spec.js
@@ -9,7 +9,7 @@
 
 const binary = require('@mapbox/node-pre-gyp');
 const path = require('path');
-const binding_path = binary.find(path.resolve(path.join(__dirname,'../../package.json')));
+const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
 const addon = require(binding_path);
 var t = require('assert');
 
