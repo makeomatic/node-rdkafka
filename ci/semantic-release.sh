f8f81be8f8f735d06a7dc37e8d171bec8d3e3982
AVVS <v@makeomatic.ca>
2022-01-29 20:20:11 -0800

    fix: use release script

---
+++ ci/semantic-release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $(git log --oneline -n 1 | grep -v "chore(release)" > /dev/null) ]]; then
+  touch .env
+  env UID=${UID} PNPM_STORE=$(pnpm config get store-dir) docker-compose --profile tests up -d
+  docker-compose exec tester pnpm i
+  docker-compose exec tester pnpm test
+  docker-compose exec tester pnpm test:e2e
+  pnpm semantic-release
+else
+  echo "skipped commit: `git log --oneline -n 1`"
+fi
