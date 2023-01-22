e55164f04372b68b2b2657845830d6e2f0c2d79e
AVVS <v@makeomatic.ca>
2023-01-22 10:05:25 -0800

    chore: force rebuild

--- ci/semantic-release.sh
+++ ci/semantic-release.sh
@@ -6,6 +6,7 @@ if git log --oneline -n 1 | grep -v "chore(release)" > /dev/null; then
   touch .env
   env UID=${UID} PNPM_STORE=$(pnpm config get store-dir) docker-compose --profile tests up -d
   docker-compose exec tester pnpm i
+  docker-compose exec tester pnpm binary:build
   docker-compose exec tester pnpm test
   docker-compose exec tester pnpm test:e2e
   pnpm semantic-release
